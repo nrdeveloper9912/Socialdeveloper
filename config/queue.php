5f6557a38248d5d9698043dd13dd7583fdce4e36
nrdeveloper9912 <nrdeveloper9912@gmail.com>
2023-02-24 21:28:16 -0600

    Update queue.php

--- config/queue.php
+++ config/queue.php
@@ -71,7 +71,7 @@ return [
     |--------------------------------------------------------------------------
     | Failed Queue Jobs
     |--------------------------------------------------------------------------
-    |
+    |agregando comenttario de prueba para hacer un git push
     | These options configure the behavior of failed queue job logging so you
     | can control which database and table are used to store the jobs that
     | have failed. You may change them to any database / table you wish.
